60a0cac1859af3d1e2c81cc57bec70fb54c73aee
VibhinnS <vibhinnsinghal25@gmail.com>
2024-08-28 19:39:59 +0530

    Added abstract class and java import stmts

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -24,6 +24,16 @@ public class GenerateAst {
         String outputDir, String baseName, List<String> types) throws IOException {
             String path = outputDir + "/" + baseName + ".java";
             PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+            writer.println("package Fox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class " + baseName + " {");
+        
+            writer.println("}");
+            writer.close();
+        
         }
 
 }
