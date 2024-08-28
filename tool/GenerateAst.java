2d3ec33d6fadf927fcd13f3e59c3b3f91233dc45
VibhinnS <vibhinnsinghal25@gmail.com>
2024-08-28 09:23:00 +0530

    Add AST file

---
+++ tool/GenerateAst.java
@@ -0,0 +1,16 @@
+package tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Usage: Generate AST <dir>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+    }
+}
