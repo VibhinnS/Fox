d2868d23067121683cbc67ef922bb6dca1fd2eda
VibhinnS <vibhinnsinghal25@gmail.com>
2024-08-28 09:43:26 +0530

    Add defineAst method

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -12,5 +12,18 @@ public class GenerateAst {
             System.exit(64);
         }
         String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary   : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal  : Object value",
+            "Unary    : Token operator, Expr right"
+        ));
+
     }
+    private static void defineAst(
+        String outputDir, String baseName, List<String> types) throws IOException {
+            String path = outputDir + "/" + baseName + ".java";
+            PrintWriter writer = new PrintWriter(path, "UTF-8");
+        }
+
 }
