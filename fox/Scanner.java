5c4376824a5c476222a15d23c0415b25ef5d12c2
VibhinnS <vibhinnsinghal25@gmail.com>
2024-08-28 08:28:03 +0530

    Added scanner hashmap - implemented basic lexical scanner

--- fox/Scanner.java
+++ fox/Scanner.java
@@ -14,6 +14,28 @@ public class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);    
+    }
+
     Scanner(String source) {
         this.source = source;
     }
@@ -65,7 +87,61 @@ public class Scanner {
             case '*':
                 addToken(STAR);
                 break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+                case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case 'o':
+                if (match('r')) {
+                    addToken(OR);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            case '/':
+                if (match('/')) {
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                    break;
+                }
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)){
+                    identifier();
+                } else {
+                    Fox.error(line, "Unexpected Char");
+                }
+                break;
         }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current ++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 
     private void addToken(TokenType type) {
@@ -80,4 +156,60 @@ public class Scanner {
     private char advance() {
         return source.charAt(current++);
     }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        
+        if (isAtEnd()) {
+            Fox.error(line, "Unterminated string.");
+            return;
+        }
+        advance();
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <='9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+                c == '_';
+      }
+    
+      private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+      }
+    
+
 }
