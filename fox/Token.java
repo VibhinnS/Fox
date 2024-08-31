3b536888105442c1d56bcd7ffd04f1c95dfcf471
VibhinnS <vibhinnsinghal25@gmail.com>
2024-08-31 19:46:31 +0530

    Made Fox methods public Fixed GenerateAst - <R> Type methods generated

--- fox/Token.java
+++ fox/Token.java
@@ -2,11 +2,11 @@ package fox;
 
 public class Token {
     final TokenType type;
-    final String lexeme;
+    public final String lexeme;
     final Object literal;
     final int line;
 
-    Token(TokenType type, String lexeme, Object literal, int line) {
+    public Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
         this.lexeme = lexeme;
         this.literal = literal;
